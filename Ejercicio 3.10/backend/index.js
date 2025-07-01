a0da78a686ac669a565c1e1f467884de04fc66a9
Angelrmatoz <angelrmatoz@gmail.com>
2025-07-01 13:12:31 -0400

    Add initial implementation of phonebook application with React and Vite
    - Created main application structure with App component
    - Implemented PersonForm for adding new contacts
    - Added Filter component for searching contacts
    - Developed Persons component to display the list of contacts
    - Integrated personServices for API interactions (CRUD operations)
    - Set up Vite configuration for React
    - Added backend server with Express to handle API requests
    - Included sample data for testing
    - Added .gitignore and README files for project setup
    - Included SVG assets for React and Vite logos

---
+++ Ejercicio 3.10/backend/index.js
@@ -0,0 +1,51 @@
+import morgan from 'morgan';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(cors());
+
+
+morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms'
+    ].join(' ')
+});
+
+const persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+];
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
